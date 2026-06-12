cdca842ac7678f8e17a4d22dfa5eeccc7c5f3920
anjan <anjananju816@gmail.com>
2026-06-12 21:01:47 +0530

    updated

---
+++ lib/prisma.ts
@@ -0,0 +1,47 @@
+// lib/prisma.ts
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+function createPrismaClient() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  });
+  return new PrismaClient({ adapter });
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
+// lib/prisma.ts
+
+// import { Pool } from "pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "./generated/prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// function createPrismaClient() {
+//   const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   });
+
+//   const adapter = new PrismaPg(pool);
+
+//   return new PrismaClient({
+//     adapter,
+//   });
+// }
+
+// export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = db;
+// }
